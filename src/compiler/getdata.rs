c21a9d42d12f00ef000a5d1b050119e033258190
liimee <git.taaa@fedora.email>
2022-07-11 05:56:17 +0700

    fix exprtoodeep

--- src/compiler/getdata.rs
+++ src/compiler/getdata.rs
@@ -1,7 +1,11 @@
 use rhai::{Engine, /*EvalAltResult*/};
 
 pub fn init_block_data(){
-    let ngn = Engine::new();
+    let mut ngn = Engine::new();
+
+    // increase if ExprTooDeep
+    ngn.set_max_expr_depths(500, 500);
+    
     // file reading needs to be replaced when compiling to wasm
     println!("{:?}",ngn.run_file("src/compiler//blockdata.rhai".into()));
 }
