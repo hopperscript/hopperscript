178ff96327e41d47cb1e539a7f83b9a8128fa1c5
Electogenius <69585636+Electogenius@users.noreply.github.com>
2022-07-14 11:57:13 +0000

    Merge from 'use-chumsky' after much suffering

--- src/compiler/types.rs
+++ src/compiler/types.rs
@@ -6,12 +6,12 @@ pub struct Fncall {
 
 #[derive(Debug)]
 pub struct Project {
-    pub variables: Vec<Variable>
+    pub variables: Vec<Variable>,
 }
 
 #[derive(Debug)]
 pub struct Variable {
     pub name: String,
     pub typ: i32,
-    pub object_id_string: String
+    pub object_id_string: String,
 }
