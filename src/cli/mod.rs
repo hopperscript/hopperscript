6c3d65dce251474b57ebfd2ab1318c7a6eb166d4
Electogenius <69585636+Electogenius@users.noreply.github.com>
2022-12-26 06:39:50 +0000

    print properly

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -32,7 +32,7 @@ fn main() {
             println!("{} {}", LANG_NAME, HELP_MENU.replace("%n", COMMAND_NAME))
         } else if args[1] == "compile" {
             // compile
-            println!("{:#?}", to_json(compile(read_file(&args[2]).as_str())));
+            println!("{}", to_json(compile(read_file(&args[2]).as_str())));
         } else {
             // unknown
             println!("Unknown option.\nUse \"{} help\" for help", COMMAND_NAME)
